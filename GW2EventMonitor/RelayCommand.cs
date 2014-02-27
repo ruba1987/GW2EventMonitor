a11955b1aba46b48eb2c7c96b391cd67d3b67afc
Russell <Russell.J.Baker@gmail.com>
2014-02-26 19:01:06 -0500

    init

---
+++ GW2EventMonitor/RelayCommand.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GW2EventMonitor
+{
+    public class RelayCommand<T> : ICommand
+    {
+        #region Fields
+
+        readonly Action<T> _execute = null;
+        readonly Predicate<T> _canExecute = null;
+
+        #endregion // Fields
+
+        #region Constructors
+
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion // Constructors
+
+        #region ICommand Members
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        #endregion // ICommand Members
+    }
+
+    public class RelayCommand : ICommand
+    {
+
+        #region private fields
+
+        private readonly Action execute;
+
+        private readonly Func<bool> canExecute;
+        private ICommand MuteCommand;
+
+        #endregion
+
+
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (this.canExecute != null)
+                    CommandManager.RequerySuggested += value;
+            }
+
+            remove
+            {
+                if (this.canExecute != null)
+                    CommandManager.RequerySuggested -= value;
+            }
+        }
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public RelayCommand(ICommand MuteCommand)
+        {
+            // TODO: Complete member initialization
+            this.MuteCommand = MuteCommand;
+        }
+
+
+
+        public void Execute(object parameter)
+        {
+            this.execute();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null ? true : this.canExecute();
+        }
+
+    }
+}
