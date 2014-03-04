5f309fc51a110ecaa1c8504df8af07c04d491db2
russell.j.baker@gmail.com <russell.j.baker@gmail.com>
2014-03-03 22:45:06 -0500

    added map monitoring and common for the observalbe dictionary. observable dictionary is not in use yet and not tested

---
+++ MapDataManager/MapDataFetcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GwApiNET;
+using GwApiNET.ResponseObjects;
+namespace MapDataManager
+{
+    public class MapDataFetcher
+    {
+
+        public MapDataFetcher()
+        {
+
+        }
+
+        public String GetMapName(int mapID)
+        {
+            return GwApi.GetMapNames()[mapID].Name;
+        }
+
+        public async Task<String> GetMapNameAsync(int mapID)
+        {
+            EntryDictionary<int, MapNameEntry> retVal = await GwApi.GetMapNamesAsync();
+            return retVal[mapID].Name;
+        }
+
+        public EntryDictionary<int, MapNameEntry> GetMapNames()
+        {
+            return GwApi.GetMapNames();
+        }
+
+        public async Task<EntryDictionary<int, MapNameEntry>> GetMapNamesAsync()
+        {
+            EntryDictionary<int, MapNameEntry> retVal = await GwApi.GetMapNamesAsync();
+            return retVal;
+        }
+
+        
+    }
+}
